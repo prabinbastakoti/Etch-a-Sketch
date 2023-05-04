5bd81b8699f34b87b9ff8d2602fa24bc0c836adb
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-05-04 23:25:32 +0545

    Add Row Column Slider with proper implementation

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let grid_container = document.querySelector('.grid');
+let color = "red";
 
-let size = 16;
+let boxSize = document.querySelector("#myRange");
+let boxValue = document.querySelector("#sizeBox");
+let boxValue2 = document.querySelector("#sizeBox1");
+
+boxValue.innerHTML = boxSize.value;
+boxValue2.innerHTML = boxSize.value;
+// Update the current slider value (each time you drag the slider handle)
+let size = boxSize.value;
 let height = 700 / size;
 let width = 700 / size;
-let color = "red";
 
 for (let i = 0; i < size; i++) {
 
@@ -15,7 +22,7 @@ for (let i = 0; i < size; i++) {
 
         let divs = document.createElement("div");
         divs.className = "square";
-        
+
         // size of each individual square divs
         divs.style.height = `${height}px`;
         divs.style.width = `${width}px`;
@@ -24,8 +31,39 @@ for (let i = 0; i < size; i++) {
 
     }
     grid_container.appendChild(container_row);
-}
 
+}
+boxSize.oninput = function () {
+    boxValue.innerHTML = this.value;
+    boxValue2.innerHTML = this.value;
+
+    grid_container.innerHTML = "";
+
+    let size = boxSize.value;
+    let height = 700 / size;
+    let width = 700 / size;
+
+    for (let i = 0; i < size; i++) {
+
+        let container_row = document.createElement("div");
+        container_row.className = "row";
+
+
+        for (let j = 0; j < size; j++) {
+
+            let divs = document.createElement("div");
+            divs.className = "square";
+
+            // size of each individual square divs
+            divs.style.height = `${height}px`;
+            divs.style.width = `${width}px`;
+
+            container_row.appendChild(divs);
+
+        }
+        grid_container.appendChild(container_row);
+    }
+}
 
 
 function changeColor(e) {
@@ -36,4 +74,6 @@ function changeColor(e) {
 
 
 }
+
+
 document.addEventListener('mouseover', changeColor);
