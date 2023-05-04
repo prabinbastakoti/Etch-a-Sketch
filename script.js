67b9cf07b127ae94d8294081f4948cc1dfd58ec5
Prabin Bastakoti <101822704+PrabinBastakoti@users.noreply.github.com>
2023-05-04 17:51:29 +0545

    Fix Constant Sketch box size and footer added

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-let grid_container = document.querySelector('.container');
+let grid_container = document.querySelector('.grid');
 
-let row = 16;
-let column = 16;
+let size = 16;
+let height = 700 / size;
+let width = 700 / size;
 let color = "red";
 
-for (let i = 0; i < row; i++) {
+for (let i = 0; i < size; i++) {
 
     let container_row = document.createElement("div");
     container_row.className = "row";
 
 
-    for (let j = 0; j < column; j++) {
+    for (let j = 0; j < size; j++) {
 
         let divs = document.createElement("div");
         divs.className = "square";
+        
+        // size of each individual square divs
+        divs.style.height = `${height}px`;
+        divs.style.width = `${width}px`;
+
         container_row.appendChild(divs);
 
     }
     grid_container.appendChild(container_row);
 }
+
 
 
 function changeColor(e) {
@@ -29,6 +36,4 @@ function changeColor(e) {
 
 
 }
-
 document.addEventListener('mouseover', changeColor);
-
